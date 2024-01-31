68ae78898cd8fe9aef3d67ea677fb561146a72b0
ktetteh <156236687+ktetteh@users.noreply.github.com>
2024-01-31 11:36:56 -0800

    fised reverseInPlace and Reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,19 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {9,8,7};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[] {7,8,9}, input2);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1,2,3,4};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[] {4,3,2,1}, ArrayExamples.reversed(input2));
   }
 }
